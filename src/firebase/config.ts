15fee1c08392003640384735b241317c042e3bb7
Harish Iyer <appsmash89@gmail.com>
2025-11-28 06:30:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1601677870-f00b7",
+  "appId": "1:668490297082:web:bf4cd9576a65ac9f4c2343",
+  "apiKey": "AIzaSyC7QgMuaY_jo5HyjkaDIhCRpFSdhK3iBZo",
+  "authDomain": "studio-1601677870-f00b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "668490297082"
+};
