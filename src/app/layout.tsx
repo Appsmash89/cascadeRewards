8206f31ef89e5c183da0b47ee1a65798479c20a8
Harish Iyer <appsmash89@gmail.com>
2025-11-29 04:25:22 +0000

    Create this file:
    /public/manifest.json
    {
      "name": "Cascade Rewards",

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
       <head>
         <title>Cascade Rewards</title>
         <meta name="description" content="Earn points for tasks and referrals." />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
