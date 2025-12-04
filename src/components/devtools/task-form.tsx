9a2bb9e1212f3fffa3d06c32f9106d8cb7a6ad71
Harish Iyer <appsmash89@gmail.com>
2025-12-04 09:10:49 +0000

    Now the contents of  dropdowns on the right of the screen are overflowin

--- src/components/devtools/task-form.tsx
+++ src/components/devtools/task-form.tsx
@@ -99,7 +99,7 @@ const ComboboxInput = ({ field, options, placeholder }: { field: any, options: (
               <ChevronsUpDown className="h-4 w-4 shrink-0 opacity-50" />
             </Button>
           </PopoverTrigger>
-          <PopoverContent className="w-[300px] p-0" align="start">
+          <PopoverContent className="w-[300px] p-0" collisionPadding={10}>
             <Command>
               <CommandInput placeholder="Search options..." />
               <CommandEmpty>No options found.</CommandEmpty>
